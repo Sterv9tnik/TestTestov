e4a595ffcdfd0ba84510aec17c5af683de14af45
Sterv9tnik <akamash@yandex.ru>
2022-08-08 13:05:47 +0300

    test2

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Before;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -8,8 +9,11 @@ public class BaseTest {
 
     @Before
     public void setUp() {
+        WebDriverManager.chromedriver().driverVersion("104").setup();
+        System.setProperty("chromeoptions.args", "--no-sandbox");
         Configuration.browser = "chrome";
-        Configuration.browserSize = "1920x1080";
+        Configuration.headless = true;
+        //Configuration.browserSize = "1920x1080";
         //Configuration.remote = "http://localhost:4444/wd/hub";
         //DesiredCapabilities capabilities = new DesiredCapabilities();
         //capabilities.setCapability("enableVNC", true);
